64a09def9bf2f0629d10166ff84598fe97208de0
Lai Ming Lam <laiming.lam@student.unsw.edu.au>
2019-09-30 15:56:48 +1000

    Add new file

---
+++ message_unreact_test.py
@@ -0,0 +1,6 @@
+def test_message_unreact(token, message_id, react_id):
+    assert message_unreact(token, message_id, react_id) == []
+    # ValueError when:
+    #  message_id is not a valid message within a channel that the authorised user has joined
+    #  react_id is not a valid React ID
+    #  Message with ID message_id does not contain an active React with ID react_id
