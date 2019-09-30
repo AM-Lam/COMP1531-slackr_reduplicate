95d1d156f18a9e44435eae654f77435b3a91b886
Lai Ming Lam <laiming.lam@student.unsw.edu.au>
2019-09-30 16:00:45 +1000

    add user_profile_test.py

--- user_profile_test.py
+++ user_profile_test.py
@@ -1,4 +1,4 @@
 def test_user_profile(token, u_id):
-    assert user_profile(token, u_id) == []
+    assert user_profile(token, u_id) == [{ email, name_first, name_last, handle_str }]
     # ValueError when:
     #  User with u_id is not a valid user
