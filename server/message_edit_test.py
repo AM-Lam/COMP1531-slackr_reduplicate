8cfbc705da2b136135dda3a57f584cd14dd4a094
z5234450 <z5234450@cse.unsw.edu.au>
2019-10-02 15:08:09 +1000

    remove extra test file

--- server/message_edit_test.py
+++
@@ -1,7 +0,0 @@
-def test_message_edit(token, message_id, message):
-    assert message_send(token, message_id, message) == []
-    # ValueError when:
-    #  Message with message_id edited by authorised user is not the poster of the message
-    #  Message with message_ is not a valid message that either 
-    #  1) is a message sent by the authorised user, or; 
-    #  2) If the authorised user is an admin, is a any message within a channel that the authorised user has joined
--- server/message_remove_test.py
+++
@@ -1,4 +0,0 @@
-def test_message_remove(token, message_id):
-    assert message_send(token, message_id) == []
-    # ValueError when:Message (based on ID) no longer exists
-    # AccessError whenUser does not have permission to remove that row
