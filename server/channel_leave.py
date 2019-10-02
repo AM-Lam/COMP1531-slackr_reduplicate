3505f7fcc04473f9dad62f0717b546a1b351e4b0
Owen Chandler <z5255778@unsw.edu.au>
2019-10-02 16:12:05 +1000

    completed channel_leave stub

--- server/channel_leave.py
+++ server/channel_leave.py
@@ -1,10 +1,76 @@
-from channel_list import channel_list
-from channel_details import channel_details
+# comment this out until these functions are written
+# from channel_list import channel_list
+# from channel_details import channel_details
+
+# placeholder functions to use until we get the real ones written
+def channel_list(token):
+    return [1, 2, 3]
+
+
+def channel_details(channel_id):
+    if channel_id == 1:
+        return {
+            'name' : "Modernists",
+            'owner_members' : [
+                {
+                    'u_id' : 113,
+                    'name_first' : "James",
+                    'name_last' : "Joyce"
+                }
+            ],
+            'all_members' : [
+                {
+                    'u_id' : 111,
+                    'name_first' : "Stephen",
+                    'name_last' : "Daedalus"
+                },
+                {
+                    'u_id' : 112,
+                    'name_first' : "Joseph",
+                    'name_last' : "Conrad"
+                },
+                {
+                    'u_id' : 113,
+                    'name_first' : "James",
+                    'name_last' : "Joyce"
+                }
+            ]
+        }
+    elif channel_id == 2:
+        return {
+            'name' : "Postmodernists",
+            'owner_members' : [
+                {
+                    'u_id' : 110,
+                    'name_first' : "Michel",
+                    'name_last' : "Foucault"
+                }
+            ],
+            'all_members' : [
+                {
+                    'u_id' : 110,
+                    'name_first' : "Michel",
+                    'name_last' : "Foucault"
+                },
+                {
+                    'u_id' : 109,
+                    'name_first' : "Giles",
+                    'name_last' : "Deleuze"
+                }
+            ]
+        }
+
+
+def get_uid_from_token(token):
+    if token == 1:
+        return 111
+    elif token == 2:
+        return 112
 
 
 def channel_leave(token, channel_id):
     # somehow get the associated uid
-    uid_ = 111
+    uid_ = get_uid_from_token(token)
 
     # check if channel exists, if it does not throw a ValueError
     if channel_id not in channel_list(token):
@@ -12,6 +78,6 @@ def channel_leave(token, channel_id):
 
     # otherwise remove the user with this token from the channel
     for index, user in enumerate(channel_details(channel_id)['all_members']):
-        if user["uid"] == uid_:
+        if user["u_id"] == uid_:
             del channel_details(channel_id)['all_members'][index]
             break
