31bc757aff64d956f1e78e79483afe09fa656ac3
Owen Chandler <z5255778@unsw.edu.au>
2019-10-04 01:34:23 +1000

    first iteration of channel_removeowner complete

--- assumptions.md
+++ assumptions.md
@@ -1,3 +1,11 @@
 - channel_leave assumptions
   - Author: Owen Chandler
   - assuming that if a user is not a part of a channel that the functions fails with no message/exception
+
+- channel_addowner assumptions
+  - Author: Owen Chandler
+  - assume that if a user owns a channel then they are a part of it
+
+- channel_removeowner assumptions
+  - Author: Owen Chandler
+  - assume that an owner CAN remove themselves as an owner of a channel
---
+++ server/channel_removeowner.py
@@ -0,0 +1,30 @@
+from channels_list import channels_list
+
+
+def channel_removeowner(token, channel_id, u_id):
+    # first get the u_id from the user token
+    owner_uid = 111
+    
+    to_add = None
+    # now check whether or not the channel actually exists, if it does exist
+    # set it to a value and break out of the loop earlier
+    for channel in channels_list(owner_uid):
+        if channel["id"] == channel_id:
+            to_add = channel
+            break
+    
+    # if the channel requested does not exist or the u_id is not an owner of it
+    # raise an error
+    if to_add == None or not channel_owner(u_id, to_add["id"]):
+        raise ValueError
+    
+    # now make sure the calling user has the permissions to make this change, we
+    # will need a few functions to be written that access a database for this to
+    # work
+    if not slackr_owner(owner_uid) and not channel_owner(owner_uid, channel_id):
+        raise AccessError("Lack permissions to add owner to this channel")
+    
+    # finally remove the u_id as an owner of the channel requested, we will need
+    # to interact with a database to handle this
+    return {}
+
