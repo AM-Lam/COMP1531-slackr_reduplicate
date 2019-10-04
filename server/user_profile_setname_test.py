f8091320caadfee333308a2620042a2306b52d3d
Antoid <z5206356@cse.unsw.edu,au>
2019-10-04 14:18:19 +1000

    Completed setname function and test, completes rudimentary tests regarding length, needs further development

--- server/user_profile_setname_test.py
+++ server/user_profile_setname_test.py
@@ -1,3 +1,23 @@
-import user_profile_setname
+import pytest
+from user_profile_setname import user_profile_setname
+from auth_register import auth_register
+
+token = "hewwo"
 
 def test_user_profile_setname():
+    user = auth_register("valid@email.com", "12345", "John", "Doe")
+
+    # this test should pass with no issue
+    assert user_profile_setname.user_profile_setname(user[token], "Jane", "Smith") == void
+
+    # trying to input a first name longer than 50 characters
+    pytest.raises(ValueError, user_profile_setname.user_profile_setname, user[token], "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", "Smith")
+
+    # trying to input a last name longer than 50 characters
+    pytest.raises(ValueError, user_profile_setname.user_profile_setname, token, "Jane", "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")
+
+    # assuming we allow mononymous names, at least one value needs to be filled (first name)
+    assert user_profile_setname.user_profile_setname(user[token], "Plato", "") == void
+
+    # trying to input an empty name
+    pytest.raises(ValueError, user_profile_setname.user_profile_setname, token, "", "")
