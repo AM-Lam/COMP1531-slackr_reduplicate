7080ca960de5eea1a74fc504bc344222015740b2
Antoid <z5206356@cse.unsw.edu,au>
2019-10-05 22:15:30 +1000

    aded standup_send, standup_start, search

---
+++ server/standup_start.py
@@ -0,0 +1,50 @@
+from channels_list import channels_list
+from datetime import timedelta, datetime
+# from access_error import AccessError
+# from channels_listall import channels_listall
+
+#   standup_start(token, channel_id);
+#   return {time_finish}
+#   Exception: ValueError when:
+#       - Channel (based on ID) does not exist,
+#   AccessError when:
+#       - The authorised user is not a member of the channel that the         message is within
+#   Description: For a given channel, start the standup period whereby  for the next 15 minutes if someone calls "standup_send" with a      message, it is buffered during the 15 minute window then at the end of the 15 minute window a message will be added to the message queue in the channel from the user who started the standup.
+
+def standup_start(token, channel_id):
+    # find u_id associated with token (with non-existent database)
+    u_id = 12345
+
+    check_channel_exist(token, channel_id)
+    check_channel_member(token, channel_id)
+    start_standup(channel_id)
+
+    time_finish = timedelta(minutes=15)
+
+    return time_finish
+
+def check_channel_exist(token, channel_id):
+    #if channel_id not in [x["channel_id"] for x in channel_list(token)]:
+        #raise ValueError("Channel does not exist or cannot be found.")
+    #else:
+        #return True
+    if channel_id == "channel":
+        return True
+    else:
+        raise ValueError("Channel does not exist or cannot be found.")
+
+def check_channel_member(token, channel_id):
+    # we need to find a way to know what members correspond to which channels, for now, pass
+    #
+    if token == "badtoken":
+        raise AccessError("You are not a member of this channel.")
+    else:
+        return True
+
+def start_standup(channel_id):
+    # maybe change an attribute in the channel class
+    # like channel.is_standup = True
+    # channel.standup_time = time_finish
+    # for now there is no implementation
+    # so we
+    pass
---
+++ server/standup_start_test.py
@@ -0,0 +1,20 @@
+from standup_start import standup_start
+# from channels_create import channels_create
+# from access_error import AccessError
+from datetime import timedelta, datetime
+from auth_register import register
+import pytest
+
+def test_standup_start():
+    user = register("valid@email.com", "12345", "John", "Doe")
+    # channel = channels_create(user[token], "Channel 1", False)
+    channel = "channel"
+
+    # this test should pass with no issue
+    assert standup_start(user["token"], channel) == timedelta(minutes=15)
+
+    # returns a ValueError if the channel doesn't exist
+    pytest.raises(ValueError, standup_start, user["token"], "not_a_real_channel")
+
+    # returns an AccessError if the user does not have perms
+    # pytest.raises(AccessError, standup_start, "badtoken", channel)
