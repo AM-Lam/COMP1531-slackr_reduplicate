aff7cd43b4e699c7e67a941cc0b19ea5c826f15b
Antoid <z5206356@cse.unsw.edu,au>
2019-10-05 20:30:26 +1000

    added change user perms, upload photo

---
+++ server/admin_userpermission_change_test.py
@@ -0,0 +1,19 @@
+from admin_userpermission_change import admin_userpermission_change
+from auth_register import auth_register
+from access_error import AccessError
+import pytest
+
+def test_admin_userpermission_change():
+    user = auth_register("valid@email.com", "12345", "John", "Doe")
+    
+    # this test should pass with no issue
+    assert admin_userpermission_change(user["token"], 12345, 1) == None
+
+    # if the user is invalid
+    pytest.raises(ValueError, admin_userpermission_change, user["token"], 54321, 1)
+
+    # if the permission is invalid
+    pytest.raises(ValueError, admin_userpermission_change, user["token"], 12345, -1)
+
+    # if the user is not an admin/owner
+    # pytest.raises(AccessError, admin_userpermission_change, "badtoken", 12345, -1)
