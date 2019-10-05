3be9ec997975999760fd041ea6d15a0c1ed74434
Arpit <a.rulania@student.unsw.edu.au>
2019-10-06 00:48:37 +1000

    some minor adjustments

--- server/auth_logout_test.py
+++ server/auth_logout_test.py
@@ -8,21 +8,18 @@ def test_validtoken1():
     # following test should fail as user is not logged in!
     dictreg = auth_register.auth_register("arpit@gmail.com", "passwording", "arpit", "rulania")
     tokenreg = dictreg["token"]
+    auth_logout.auth_logout(tokenreg)
     pytest.raises(ValueError, auth_logout.auth_logout, tokenreg)
 
 # validtoken test should pass
 def test_validtoken(tokenlog):
     dictreg = auth_register.auth_register("arpit@gmail.com", "passwording", "arpit", "rulania")
-    dictlog = auth_login.auth_login("arpit@gmail.com", "passwording")
-    tokenlog = dictlog["token"]
+    tokenlog = dictreg["token"]
     assert(auth_logout.auth_logout(tokenlog)) #assuming a successful logout returns a true boolean
 
 # invalid token test should fail
 def test_invalidtoken(tokenlog):
     dictreg = auth_register.auth_register("arpit@gmail.com", "passwording", "arpit", "rulania")
-    dictlog = auth_login.auth_login("arpit@gmail.com", "passwording")
-    tokenlog = dictlog["token"]
+    tokenlog = "invalid"
     pytest.raises(ValueError, auth_logout.auth_logout, tokenlog)
 
-
-## assuming auth_register does not login the person
