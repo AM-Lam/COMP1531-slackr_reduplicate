2477a8d466fe5d750e3d2663d5e7cf47f54b8e82
Owen Chandler <z5255778@unsw.edu.au>
2019-10-14 15:34:11 +1100

    very basic setup, also testing vscode ssh

--- server/auth_logout.py
+++
@@ -1,3 +0,0 @@
-def auth_logout(token):
-    # need to invalidate the token not possible for iteration 1
-    return {}  #returns an empty dictionary!!
--- server/channels_listall.py
+++
@@ -1,7 +0,0 @@
-def channels_listall(token):
-    # this is pretty simple, just grab all the channels from the database, reall
-    # -y not a lot I can write at this point of the project
-    channels = []
-    
-    return { "channels" : channels }
-
