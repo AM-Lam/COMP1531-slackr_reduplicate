18235e389cc9075707d891bce95e31261bb1422d
z5234450 <z5234450@unsw.edu.au>
2019-10-16 16:17:43 +1100

    basic setup

--- server/setup.py
+++ server/setup.py
@@ -1,3 +1,24 @@
 from modules import *
 
+
+DATABASE = None
+
+
+def get_data():
+    global DATABASE
+    return DATABASE
+
+
+def update_data(new_database):
+    global DATABASE
+    DATABASE = new_database
+    return DATABASE
+
+
+# initialise an empty database
+update_data({
+    "users" : [],
+    "channels" : []
+})
+
 print("Setup complete")
