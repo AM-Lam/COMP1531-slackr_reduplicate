5e8dc38d93c4283560db227316b635abc92562af
Antoid <z5206356@cse.unsw.edu,au>
2019-10-22 18:52:23 +1100

    what

--- server/search.py
+++ server/search.py
@@ -1,3 +1,5 @@
+import jwt
+
 #   search(token, query_str);
 #   return {messages}
 #   Exception: N/A
@@ -5,9 +7,19 @@
 
 def search(token, query_str):
     # find u_id associated with token (with non-existent database)
-    u_id = 12345
+    admin_user_id = check_valid_token(token)
 
     # normally we would pull messages from a list/dictionary of all messages but since that isn't implemented, we just return nothing
     messages = []
     
     return messages
+
+def check_valid_token(token):
+    # find the user ID associated with this token, else raise a ValueError
+    decoded_jwt = jwt.decode(token, 'sempai', algorithms=['HS256'])
+    try:
+        for x in database:
+            if x.get("u_id") == decoded_jwt.key():
+                return x.get("u_id")
+    except Exception as e:
+        raise ValueError("token invalid")
--- server/user_profile_setemail.py
+++ server/user_profile_setemail.py
@@ -1,4 +1,5 @@
 import re
+import jwt
 
 #   user_profile_setemail(token, email);
 #   return void
@@ -8,12 +9,23 @@ import re
 #   Description: Update the authorised user's email address
 
 def user_profile_setemail(token, email):
-    u_id = 12345
+    # find u_id associated with token (with non-existent database)
+    admin_user_id = check_valid_token(token)
 
     check_if_email_valid(email)
     check_email_database(email)
     change_email(u_id, email)
     return
+
+def check_valid_token(token):
+    # find the user ID associated with this token, else raise a ValueError
+    decoded_jwt = jwt.decode(token, 'sempai', algorithms=['HS256'])
+    try:
+        for x in database:
+            if x.get("u_id") == decoded_jwt.key():
+                return x.get("u_id")
+    except Exception as e:
+        raise ValueError("token invalid")
 
 def check_if_email_valid(email):
     regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
