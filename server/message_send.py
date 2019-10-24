e83787d6839fe3bffe66e59d8903895d4eb3230a
z5234450 <z5234450@unsw.edu.au>
2019-10-24 16:10:56 +1100

    database remain the same

--- server/message_send.py
+++ server/message_send.py
@@ -1,4 +1,5 @@
 from .database import *
+from datetime import datetime 
 import jwt
 
 
@@ -7,30 +8,31 @@ def message_send(token, channel_id, message):
     if len(message) > 1000:
         raise ValueError# ("The message is too long. Please keep it within 1000 characters.")   
     
-    # if the user id not a member of the channel
-    # TODO
-    # verify token
-    if permission_id_dic[token][channel_id] == None:
-        raise AccessError #("You don't have access in this channel. Please try again after you join.")
+    # not an authorised user
+    if token not in update_data['token']:
+        raise AccessError 
 
     # now grab the u_id associated with the provided token
     token_payload = jwt.decode(token, get_secret(), algorithms=["HS256"])
     u_id = token_payload["u_id"]
+
+    # if the user id not a member of the channel
+    if u_id in update_data['channels']['members']:
+        raise AccessError #("You don't have access in this channel. Please try again after you join.")
     
     server_data = get_data()
 
     # make our message id just be the count of messages we already have
     # incremented by 1, this way the message_ids will increase sequentially
     message_id = len(server_data["message"]) + 1
+    time_sent = str(datetime.now())
 
-    # TODO
-    time_sent=
     # at the start there will be no messages and the only member will be
     # the creator of the channel
-    new_message = Messages(message, [u_id], message, channel_id, time_sent)
+    new_message = Messages(message_id, u_id, message, channel_id, time_sent)
     
     # add the message to the server database
-    server_data["message"].append(new_message)
+    server_data["channel"]['_messages'].append(message)
 
     # return the new message's id
     return { "message_id" : message_id }
--- server/message_send_test.py
+++ server/message_send_test.py
@@ -25,11 +25,12 @@ def test_message_send():
     }
 
     db = get_data()
+
     
-    # try to create a valid, public message
+    
+    # try to create a valid message
     # TODO: get the channel_id
     message_1 = message_send(user1["token"], 1, "Hello")
-    
     # check that the channel exists
     assert message_1 is not None
 
@@ -46,11 +47,9 @@ def test_message_send():
     
     # reset message_1
     message_1 = None
-    
     # try to create a channel with an invalid name
     pytest.raises(ValueError, message_send, user1["token"], 111,
                 "bfiDw25miCyBvrwSfYWVRTSdunVfTxzWRfkYrvOztvF3BrtCHbXWoIKPbpBhYdQsWf7TTWQ5Z0cKmBBygwVRAyS9Yt6YBtitWYQDVVVembsy7izJMjuVk611l7NmxPWXw8w8pk7EtKHq1464icy6z5qcnG6cSALJUT86hU2tXG3redpcINHEG9BaNTqgngUSGNENIVkD9mzYRFeChjq7CBwZVP7G0yCYxn3M3VSgUIyFbQElRGkKtX7KWvg9FxlFXnld8ScXaTE27i8zT4FqZMf8j45wVYZOJLRpOnX1JaDvfvpMfG5ybCleIQYe9GADEzcP7RxBcs7EaIqSRmpXOZl3kyJDdYubEttcR9rrulQo3dJMzPa3WnaosCByZCEeO3rqHqryXcpwlB8Z5ztYGJ8fiRDMgwloBjjAZyHPU6CTd76zFRXPyywnjzQhmsBVHQ6qjyNQODV9j2mLejkCVjFXgVYbOfQnSQZALo7PDkB4BEQDJWY4GVtG7FfhhGM9YsolHDWzQnGkFjvPfLgI1rXNsacFr2jbb5qv7C8nCnE63BDroyI0UjR0iQqFuNIuJI2pxuibdxZcsifRFjhf3j86Ya03mSBBxoULl8o9mOZx60nYalsQcBGd56qavrrLIqxWB6KOhLoFhSwOyZCc4bU3s4UX8oN0s9BXaeAM7S6dyLbI8qA6OKJFHmpUielq6dlaaSXHOZMmOgG5K6lmpUvaxc9vz0AUmgWTo8ENHv1iHFoqEDgWsjbziBkRaxxCCnyoE6qg00OvNxerVNLQgEYYVj9TBeEPbXlDCQSvx50RAEbE8PxRsf9Fp0ZvdqXcyOr3ZtZ6X20afRnkZx8bcll7UF7LKBJl3BFrRGcpKoT5rUpAEifG3tMNB1jANgrJSbdkh4fwkCRfX1n52VyO9xnXTqijFmr1zMg01SpuSRTcqu901OA4kOQZzh0iQk1baCQDEOrFtDSuSLtXTYXl6nS5upAK0v8AnRxtlsdflhyge")
-
 
 
 # def test_message_send(token, channel_id, message):
