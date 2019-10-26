1a2499b2706224130410893766a3e48bbcd4cc87
z5234450 <z5234450@unsw.edu.au>
2019-10-26 16:37:58 +1100

    class channel change

--- server/database.py
+++ server/database.py
@@ -51,7 +51,7 @@ class User:
 
 
 class Channel:
-    def __init__(self, channel_id, channel_name, messages, members, public):
+    def __init__(self, channel_id, channel_name, messages, creator, public):
         self._channel_id = channel_id       # id of the channel, increases 
                                             # sequentially
         
@@ -60,8 +60,11 @@ class Channel:
         self._messages = messages           # messages in the channel, list
                                             # of Message objects
         
-        self._members = members             # members of the channel, dictionary
-                                            # with u_id as key and perm. as value
+        self._members = creator             # members of the channel, just a list
+                                            # of u_ids
+        
+        self._owners = creator              # owners of the channel, initially set to
+                                            # the creator of the channel
         
         self._public = public               # is the channel public, boolean 
                                             # val
@@ -72,17 +75,10 @@ class Channel:
             "channel_id" : self._channel_id,
             "channel_name" : self._channel_name,
             "messages" : self._messages,
+            "owners" : self._owners,
             "members" : self._members,
             "public" : self._public,
         }
-    
-
-    def update_channel_data(self, new_data):
-        self._channel_id = new_data["channel_id"]
-        self._first_name = new_data["first_name"]
-        self._last_name = new_data["last_name"]
-        self._password = new_data["password"]
-        self._email = new_data["email"]
         
 
     def frontend_format(self):
@@ -90,7 +86,6 @@ class Channel:
             "channel_id" : self._channel_id,
             "name" : self._channel_name
         }
-
  
 class Messages:
     def __init__(self, message_id, u_id, text, channel_id, time_sent, reacts):
