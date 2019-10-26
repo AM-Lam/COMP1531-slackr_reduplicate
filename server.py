5878540a3a6e247ce42aff86bf010eebd2059322
Anthony Wallace <anthony.wallace@student.unsw.edu.au>
2019-10-27 01:32:59 +1100

    Replace server.py

--- server.py
+++ server.py
@@ -12,15 +12,15 @@ from server import *
 
 def defaultHandler(err):
     response = err.get_response()
-    
+
     response.data = dumps({
         "code": err.code,
         "name": "System Error",
         "message": err.description,
     })
-    
+
     response.content_type = 'application/json'
-    
+
     return response
 
 
@@ -98,7 +98,7 @@ def echo2():
 
 @APP.route('/channels/create', methods=["POST"])
 def run_channels_create():
-    """ 
+    """
         run the channels_create function to make a new channel and
         add it to the  server database
     """
@@ -106,7 +106,7 @@ def run_channels_create():
     return_value = channels_create.channels_create(request_data["token"],
                                                    request_data["name"],
                                                    bool(request_data["is_public"]))
-    
+
     return dumps(return_value)
 
 
@@ -115,7 +115,7 @@ def run_channel_leave():
     request_data = request.get_json()
     return_value = channel_leave.channel_leave(request_data["token"],
                                                request_data["channel_id"])
-    
+
     return dumps(return_value)
 
 
@@ -129,7 +129,7 @@ def run_channels_listall():
     """
     request_data = request.get_json()
     return_value = channels_listall.channels_listall(request_data["token"])
-    
+
     return dumps(return_value)
 
 
@@ -139,7 +139,7 @@ def run_channels_list():
     return_value = channels_list.channels_list(
         request_data["token"]
     )
-    
+
     return dumps(return_value)
 
 
@@ -147,10 +147,10 @@ def run_channels_list():
 def run_channel_join():
     request_data = request.get_json()
     return_value = return_value = channel_join.channel_join(
-        request_data["token"], 
+        request_data["token"],
         request_data["channel_id"]
     )
-    
+
     return dumps(return_value)
 
 
@@ -191,6 +191,92 @@ def run_message_sendlater():
 
     return dumps(return_value)
 
+@APP.route('/user/profile/setname', methods=["PUT"])
+def run_profile_setname():
+    request_data = request.get_json()
+    return_value = user_profile_setname.user_profile_setname(
+        request_data["token"],
+        request_data["name_first"],
+        request_data["name_last"]
+    )
+
+    return dumps(return_value)
+
+@APP.route('/user/profile/setemail', methods=["PUT"])
+def run_profile_setemail():
+    request_data = request.get_json()
+    return_value = user_profile_setemail.user_profile_setemail(
+        request_data["token"],
+        request_data["email"],
+    )
+
+    return dumps(return_value)
+
+@APP.route('/user/profile/sethandle', methods=["PUT"])
+def run_profile_sethandle():
+    request_data = request.get_json()
+    return_value = user_profile_sethandle.user_profile_sethandle(
+        request_data["token"],
+        request_data["handle_str"],
+    )
+
+    return dumps(return_value)
+
+@APP.route('/user/profile/uploadphoto', methods=["POST"])
+def run_profile_uploadphoto():
+    request_data = request.get_json()
+    return_value = user_profile_uploadphoto.user_profile_uploadphoto(
+        request_data["token"],
+        request_data["img_url"],
+        request_data["x_start"],
+        request_data["y_start"],
+        request_data["x_end"],
+        request_data["y_end"],
+    )
+
+    return dumps(return_value)
+
+@APP.route('/standup/start', methods=["POST"])
+def run_standup_start():
+    request_data = request.get_json()
+    return_value = standup_start.standup_start(
+        request_data["token"],
+        request_data["channel_id"],
+    )
+
+    return dumps(return_value)
+
+@APP.route('/standup/send', methods=["POST"])
+def run_standup_send():
+    request_data = request.get_json()
+    return_value = standup_send.standup_send(
+        request_data["token"],
+        request_data["channel_id"],
+        request_data["message"],
+    )
+
+    return dumps(return_value)
+
+@APP.route('/search', methods=["GET"])
+def run_search():
+    request_data = request.get_json()
+    return_value = search.search(
+        request_data["token"],
+        request_data["query_str"],
+    )
+
+    return dumps(return_value)
+
+@APP.route('/admin/userpermission/change', methods=["POST"])
+def run_admin_userpermission_change():
+    request_data = request.get_json()
+    return_value = admin_userpermission_change.admin_userpermission_change(
+        request_data["token"],
+        request_data["u_id"],
+        request_data["permission_id"],
+    )
+
+    return dumps(return_value)
 
 if __name__ == '__main__':
     APP.run(port=(sys.argv[1] if len(sys.argv) > 1 else 5000))
