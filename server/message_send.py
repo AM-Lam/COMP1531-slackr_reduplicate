f858e045f0bc1846fc001e0e865d1b250df26d7d
z5234450 <z5234450@unsw.edu.au>
2019-10-27 03:49:19 +1100

    user.profile done

--- server/message_send.py
+++ server/message_send.py
@@ -1,46 +1,47 @@
 import jwt
+import threading
 from .database import *
 from datetime import datetime 
-from .access_error import AccessError
-from .channels_list import channels_list
+from .access_error import *
 
 def message_send(token, channel_id, message):
-    # Message is more than 1000 characters
-    if len(message) > 1000:
-        raise ValueError
-    
     server_data = get_data()
 
-    # not an authorised user
-    if token not in server_data['token']:
-        raise AccessError 
+    # if the token is not valid raise an AccessError
+    if not server_data["tokens"].get(token, True):
+        raise AccessError(description="This token is invalid")
 
     # now grab the u_id associated with the provided token
     token_payload = jwt.decode(token, get_secret(), algorithms=["HS256"])
     u_id = token_payload["u_id"]
-
-    # not an authorised user
-    if token not in server_data['token']:
-        raise AccessError 
-
-    # if the user id not a member of the channel
-    if u_id not in channels_list(list):
-        raise AccessError 
     
-    # make our message id just be the count of messages we already have
-    # incremented by 1, this way the message_ids will increase sequentially
-    message_id = len(server_data["message"]) + 1
-    time_sent = str(datetime.now())
+    # Message is more than 1000 characters
+    if len(message) > 1000:
+        raise ValueError(description="Messages must be less than 1000 characters")
 
-    # at the start there will be no messages and the only member will be
-    # the creator of the channel
-    new_message = Messages(message_id, u_id, message, channel_id, time_sent, [])
-    
-    channels_list = channels_list(token)
+    channel_ = None
     # add the message to the server database
-    for channel in channels_list:
-        if channel.channel_id == channel_id:
-            channel.messages.append(new_message)
+    for channel in server_data["channels"]:
+        if channel.get_id() == channel_id:
+            channel_ = channel
+            break
+    
+    if channel_ is None:
+        raise ValueError(description="Channel does not exist")
+
+    if u_id not in channel_.get_members():
+        raise AccessError(description="You don't have access in this channel")
+        
+    # now create the message we will be sending
+    message_id = channel_.get_m_id()
+    to_send = Messages(message_id, u_id, message, channel_id,
+                       datetime.utcnow(), [])
+    
+    # increment the channel's max message id
+    channel_.increment_message_id()
+    
+    # start a thread that will call send_message
+    channel_.add_message(to_send)
 
     # return the new message's id
     return { "message_id" : message_id }
--- server/message_send_test.py
+++ server/message_send_test.py
@@ -1,6 +1,7 @@
 import pytest
 import jwt
 from .database import *
+from .access_error import *
 from .auth_register import auth_register
 from .channels_create import channels_create
 from .message_send import message_send
@@ -15,14 +16,23 @@ def test_message_send():
     # user1 = auth_register("valid@email.com", "1234", "Bob", "Jones")
 
     # just got the u_id by putting fake data into jwt.io
+    secret = get_secret()
     user1 = {
-        "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1X2lkIjoiMTExIn0.dyT88tdeqRfTRsfjQRenygNT_ywC-wTAFWlvMUHfhxI"
+        "token" : jwt.encode({"u_id" : "111"}, secret, algorithm="HS256").decode(),
+        "u_id" : "111"
     }
 
+    user2 = {
+        "token" : jwt.encode({"u_id" : "112"}, secret, algorithm="HS256").decode(),
+        "u_id" : "112"
+    }
 
+    user3 = {
+        "token" : jwt.encode({"u_id" : "113"}, secret, algorithm="HS256").decode(),
+        "u_id" : "113"
+    }
+    
     channel_id = channels_create(user1["token"], "Channel 1", True)
-
-    db = get_data()
 
     # try to create a valid message
     message_1 = message_send(user1["token"], channel_id, "Hello")
@@ -43,32 +53,15 @@ def test_message_send():
                         }
                         )
     
+    # the user is not a member in the group
+    pytest.raises(ValueError, message_send, user3["token"], 1,
+                "Hello")
+
     # reset message_1
     message_1 = None
     # the message is over 1000 characters
     pytest.raises(ValueError, message_send, user1["token"], 111,
                 "bfiDw25miCyBvrwSfYWVRTSdunVfTxzWRfkYrvOztvF3BrtCHbXWoIKPbpBhYdQsWf7TTWQ5Z0cKmBBygwVRAyS9Yt6YBtitWYQDVVVembsy7izJMjuVk611l7NmxPWXw8w8pk7EtKHq1464icy6z5qcnG6cSALJUT86hU2tXG3redpcINHEG9BaNTqgngUSGNENIVkD9mzYRFeChjq7CBwZVP7G0yCYxn3M3VSgUIyFbQElRGkKtX7KWvg9FxlFXnld8ScXaTE27i8zT4FqZMf8j45wVYZOJLRpOnX1JaDvfvpMfG5ybCleIQYe9GADEzcP7RxBcs7EaIqSRmpXOZl3kyJDdYubEttcR9rrulQo3dJMzPa3WnaosCByZCEeO3rqHqryXcpwlB8Z5ztYGJ8fiRDMgwloBjjAZyHPU6CTd76zFRXPyywnjzQhmsBVHQ6qjyNQODV9j2mLejkCVjFXgVYbOfQnSQZALo7PDkB4BEQDJWY4GVtG7FfhhGM9YsolHDWzQnGkFjvPfLgI1rXNsacFr2jbb5qv7C8nCnE63BDroyI0UjR0iQqFuNIuJI2pxuibdxZcsifRFjhf3j86Ya03mSBBxoULl8o9mOZx60nYalsQcBGd56qavrrLIqxWB6KOhLoFhSwOyZCc4bU3s4UX8oN0s9BXaeAM7S6dyLbI8qA6OKJFHmpUielq6dlaaSXHOZMmOgG5K6lmpUvaxc9vz0AUmgWTo8ENHv1iHFoqEDgWsjbziBkRaxxCCnyoE6qg00OvNxerVNLQgEYYVj9TBeEPbXlDCQSvx50RAEbE8PxRsf9Fp0ZvdqXcyOr3ZtZ6X20afRnkZx8bcll7UF7LKBJl3BFrRGcpKoT5rUpAEifG3tMNB1jANgrJSbdkh4fwkCRfX1n52VyO9xnXTqijFmr1zMg01SpuSRTcqu901OA4kOQZzh0iQk1baCQDEOrFtDSuSLtXTYXl6nS5upAK0v8AnRxtlsdflhyge")
-
-def test_Msend_invalid_user():
-    # user1 = auth_register("valid@email.com", "1234", "Bob", "Jones")
-
-    # just got the u_id by putting fake data into jwt.io
-    user1 = {
-        "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1X2lkIjoiMTExIn0.dyT88tdeqRfTRsfjQRenygNT_ywC-wTAFWlvMUHfhxI"
-    }
-    user2 = {
-        "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1X2lkIjoiMjIifQ.V8RNVCtIW66E7gxk54-FYE_XRp67TsndcrCmZMfJ0RI"
-    }
-    user3 = {
-        "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1X2lkIjoiMyJ9.QaiuthhOZ3vU8iRd7QDtbs89nDHpNo6lKgo_JPwpSj4"
-    }
-
-    # channel_id = channels_create("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1X2lkIjoiMTExIn0.dyT88tdeqRfTRsfjQRenygNT_ywC-wTAFWlvMUHfhxI", "channel1", True)
-    # db = get_data()
-
-    # the user is not a member in the group
-    pytest.raises(ValueError, message_send, user3["token"], 1,
-                "Hello")
 
 
 # def test_message_send(token, channel_id, message):
