69db339707d0d505c38aa189cff678f779b21dda
Anthony Wallace <anthony.wallace@student.unsw.edu.au>
2019-10-26 23:55:51 +1100

    Replace user_profile_sethandle.py

--- server/user_profile_sethandle.py
+++ server/user_profile_sethandle.py
@@ -1,3 +1,6 @@
+from .access_error import AccessError
+import jwt
+
 #   user_profile_sethandle(token, handle_str);
 #   return void
 #   Exception: ValueError when:
@@ -6,13 +9,29 @@
 
 def user_profile_sethandle(token, handle_str):
     # find u_id associated with token (with non-existent database)
-    u_id = 12345
+    u_id = check_valid_token(token)
+    
     handle_check(handle_str)
     handle_in_use_check(handle_str)
     change_handle(u_id, handle_str)
 
     return
 
+def check_valid_token(token):
+    # find the user ID associated with this token, else raise a ValueError
+    global DATABASE
+    global SECRET
+
+    token = jwt.decode(token, SECRET, algorithms=['HS256'])
+
+    try:
+        for x in DATABASE["users"]:
+            user_id = x.get_u_id()
+            if user_id == token["u_id"]:
+                return user_id
+    except Exception as e:
+        raise ValueError("token invalid")
+        
 def handle_check(handle_str):
     if len(handle_str) < 20 and len(handle_str) > 0:
         return True
@@ -20,13 +39,22 @@ def handle_check(handle_str):
         raise ValueError("Handle must be between 1 and 20 characters")
 
 def handle_in_use_check(handle_str):
-    # when database is implemented, check if the handle is already being used
-    # since the database doesn't exist yet, just pretend the test input "handle1" is already in use
-    if handle_str != "handle1":
-        return True
-    else:
-        raise ValueError("Handle is already in use.")
+    # check if the handle is already being used/exists within the database
+    global DATABASE
+    for x in DATABASE["handle"]:
+        y = x.get_user_data()
+        if y.get("handle") == handle_str:
+            raise ValueError("Handle is already in use.")
+    return True
 
 def change_handle(u_id, handle_str):
-    # change first and last name in the database (which doesn't exist yet)
-    pass
+    # change handle in the database for associated user
+    global DATABASE
+    try:
+        for x in DATABASE["users"]:
+            y = x.get_user_data()
+            if y.get("u_id") == u_id:
+                DATABASE.update_user_data({"handle": handle})
+                break
+    except Exception as e:
+        raise ValueError("Error: Couldn't change handle.")
