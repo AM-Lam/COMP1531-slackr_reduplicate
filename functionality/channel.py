b8fc4d412d4d7d6f34a45b6047c72a681819c096
Owen Chandler <z5255778@unsw.edu.au>
2019-11-03 20:08:38 +1100

    channel.py style fixed + got message_sendlater working

--- functionality/channel.py
+++ functionality/channel.py
@@ -1,8 +1,9 @@
 """
 Functions that relate to the creation, modification and deletion of channels.
 """
-import jwt
-from .database import *
+from .database import (get_data, check_valid_token, get_channel, get_user,
+                       is_user_member, is_user_owner, get_message_list,
+                       is_valid_u_id, message_count, Channel)
 from .access_error import *
 
 
@@ -10,67 +11,50 @@ def channel_addowner(token, channel_id, u_id):
     """
     Select a user by u_id and add them to the channel owners of this channel
     """
-    server_data = get_data()
 
-    # first check whether or not the token is valid, since
-    # auth_register is not yet complete just always pass this test
-    if not server_data["tokens"].get(token, False):
-        raise AccessError("Token not valid")
+    # get the u_id of the requesting user and the channel object
+    owner_u_id = check_valid_token(token)
+    to_add = get_channel(channel_id)
 
-    # first get the u_id from the user token
-    token_payload = jwt.decode(token, get_secret(), algorithms=["HS256"])
-    owner_u_id = token_payload["u_id"]
-
-    to_add = None
-    # now check whether or not the channel actually exists, if it does
-    # exist set it to a value and break out of the loop earlier
-    for channel in server_data["channels"]:
-        if channel.get_id() == channel_id:
-            to_add = channel
-            break
-
-    # if the channel requested does not exist or the u_id is already an
-    # owner of it raise an error, this will probably need to interact
-    # with a database
-    if to_add is None:
-        raise ValueError(description="The channel requested does not exist")
-
-    if u_id in to_add.get_owners():
+    if is_user_owner(u_id, channel_id):
         raise ValueError(description="The user is already an owner in this channel")
 
     # now make sure the calling user has the permissions to make this
-    # change, we will need a few functions to be written that access a
-    # database for this to work
-    if owner_u_id not in to_add.get_owners():
+    # change
+    if not is_user_owner(owner_u_id, channel_id):
         raise AccessError(description="Lack permissions to add owner to this channel")
 
-    # finally set the u_id to be an owner of the channel requested
+    # finally set u_id to be an owner of the channel requested, if they
+    # are not already a member of this channel add them as one as well
     to_add.get_owners().append(u_id)
-    if u_id not in to_add.get_members():
+    if not is_user_member(u_id, channel_id):
         to_add.get_members().append(u_id)
 
     return {}
 
 
 def channel_details(token, channel_id):
+    """
+    Select a channel by id and return its details.
+    """
     u_id = check_valid_token(token)
+
+    if not is_user_member(u_id, channel_id):
+        raise AccessError(description='You do not have permission to do this')
+
     channel = get_channel(channel_id)
-    
     channel_members = channel.get_members()
 
-    if u_id not in channel_members:
-        raise AccessError(description='You do not have permission to do this')
-    
     # TODO: Rewrite this to not be awful, will likely require
     # rethinking our data structures
-    
+
     # convert the channel_members list to a form the frontend can read
     channel_members = [{
         "u_id" : user.get_u_id(),
         "name_first" : user.get_first_name(),
         "name_last" : user.get_last_name()
     } for user in get_data()["users"] if user.get_u_id() in channel_members]
-    
+
     # do the same thing with the channel owners
     channel_owners = channel.get_owners()
     channel_owners = [{
@@ -78,13 +62,18 @@ def channel_details(token, channel_id):
         "name_first" : user.get_first_name(),
         "name_last" : user.get_last_name()
     } for user in get_data()["users"] if user.get_u_id() in channel_owners]
-    
-    return {"name" : channel.get_name(), 
+
+    return {"name" : channel.get_name(),
             "owner_members" : channel_owners,
             "all_members" : channel_members}
 
 
 def channel_invite(token, channel_id, u_id):
+    """
+    Select a channel by id and user by u_id, check whether or not we
+    have permission to add a new user to a channel and, if we do, add
+    the new user to the list of channel members.
+    """
     inviter_id = check_valid_token(token)
     channel = get_channel(channel_id)
 
@@ -93,73 +82,61 @@ def channel_invite(token, channel_id, u_id):
 
     if is_user_member(u_id, channel_id):
         raise ValueError(description="User is already a member of this channel")
-    
+
     if not is_user_owner(inviter_id, channel_id):
         raise AccessError(description="You do not have permission to do this")
-    
+
     channel.add_member(u_id)
 
     return {}
 
 
 def channel_join(token, channel_id):
-    server_data = get_data()
+    """
+    Select a channel by id and, if it is public or we have special
+    permissions, join it as a member.
+    """
+    u_id = check_valid_token(token)
+    user = get_user(u_id)
 
-    # if the token is invalid throw an access error, since
-    # auth_register is not complete just assume all tokens
-    # are valid
-    if not server_data["tokens"].get(token, False):
-        raise AccessError(description="This token is invalid")
+    channel = get_channel(channel_id)
 
-    token_payload = jwt.decode(token, get_secret(), algorithms=["HS256"])
-    u_id = token_payload["u_id"]
-
-    # next we have to check whether or not the given user is an admin,
-    found_channel = False
-
-    for channel in server_data["channels"]:
-        # somehow check if the channel is private, another database
-        # interaction it should probably look like what's below
-        if channel.get_id() == channel_id:
-            # we currently have no checks forglobal admins
-            if not channel.is_public():
-                raise AccessError("Cannot join private channel as regular\
+    if not channel.is_public() and not (user.is_global_admin() or
+                                        user.is_slackr_owner()):
+        raise AccessError("Cannot join private channel as regular\
                                    user")
-            found_channel = True
-            break
 
-    if not found_channel:
-        raise ValueError
-
-    # add the user's u_id to the channel's list of members, since the
-    # data we get using channels_listall appears to be read-only
-    channel.get_members().append(u_id)
+    channel.add_member(u_id)
 
     return {}
 
 
 def channel_leave(token, channel_id):
-    # somehow get the associated uid, this will presumably need
-    # to interact with a database or something similar
-    token_payload = jwt.decode(token, get_secret(), algorithms=["HS256"])
-    u_id = token_payload["u_id"]
+    """
+    Take a channel by id and, if we are a member, leave the channe.
+    Ensure that member's also lose their owner status, if the user is
+    not a member fail silently.
+    """
+    u_id = check_valid_token(token)
+    channel = get_channel(channel_id)
 
-    server_data = get_data()
+    if is_user_member(u_id, channel_id):
+        channel.get_members().remove(u_id)
 
-    # try to remove the user from the channel, if they are not in the
-    # channel then just return without any error
-    for c in server_data["channels"]:
-        if c.get_id() == channel_id:
-            if u_id in c.get_members():
-                c.get_members().remove(u_id)
-            return {}
+    if is_user_owner(u_id, channel_id):
+        channel.get_owners().remove(u_id)
 
-    # if we get here the channel does not exist and we need to raise an
-    # exception
-    raise ValueError
+    return {}
 
 
-def channel_messages(token, channel_id, start):    
+def channel_messages(token, channel_id, start):
+    """
+    Retrieve 50 messages from the channel referred to by channel_id
+    beginning from start. Make sure messages are listed in
+    chronological order and if there are no messages left set end to -1
+    on the return.
+    """
+
     channel = get_channel(channel_id)
     u_id = check_valid_token(token)
 
@@ -186,33 +163,19 @@ def channel_messages(token, channel_id, start):
 
 
 def channel_removeowner(token, channel_id, u_id):
-    server_data = get_data()
+    """
+    Select a channel by id and a user by id then, if we have the perms,
+    remove that user from the list of channel owners. If the user is
+    not an owner raise a ValueError and if we do not have permissions
+    to do this raise an AccessError.
+    """
 
-    if not server_data["tokens"].get(token, False):
-        raise AccessError(description="This token is invalid")
+    # get the data about the requesting user
+    owner_uid = check_valid_token(token)
+    owner = get_user(owner_uid)
 
-    # first get the u_id from the user token
-    token_payload = jwt.decode(token, get_secret(), algorithms=["HS256"])
-    owner_uid = token_payload['u_id']
-
-    owner = None
-    for u in server_data["users"]:
-        if u.get_u_id() == owner_uid:
-            owner = u
-            break
-
-    to_remove = None
-    # now check whether or not the channel actually exists, if it does
-    # exist set it to a value and break out of the loop earlier
-    for channel in server_data["channels"]:
-        if channel.get_id() == channel_id:
-            to_remove = channel
-            break
-
-    # if the channel requested does not exist or the u_id is not an
-    # owner of it raise an error
-    if to_remove is None:
-        raise ValueError(description="The requested channel does not exist")
+    # get the channel
+    to_remove = get_channel(channel_id)
 
     if u_id not in to_remove.get_owners():
         raise ValueError(description="The user is not an owner of this channel")
@@ -229,20 +192,25 @@ def channel_removeowner(token, channel_id, u_id):
 
 
 def channels_create(token, name, is_public):
+    """
+    Create a new channel with the given name, if is_public is true make
+    the channel public otherwise make it private. Return the id of the
+    newly created channel.
+    """
+
     # first check for a valid name
     if len(name) > 20:
         raise ValueError(description="Channel name is too short")
 
-    # now grab the u_id associated with the provided token
-    token_payload = jwt.decode(token, get_secret(), algorithms=["HS256"])
-    u_id = token_payload["u_id"]
+    # get the u_id of this user
+    u_id = check_valid_token(token)
 
     server_data = get_data()
 
     # make our channel id just be the count of channels we already have
     # incremented by 1, this way the channel_ids will increase
     # sequentially
-    channel_id = len(server_data["channels"])
+    channel_id = len(server_data["channels"]) + 1
 
     # at the start there will be no messages and the only member will
     # be the creator of the channel
@@ -256,17 +224,18 @@ def channels_create(token, name, is_public):
 
 
 def channels_list(token):
+    """
+    Return a list of all the channels that the user making this request
+    belongs to.
+    """
+
     # this is pretty simple, just grab the "database"
     database = get_data()
 
-    # if the token is not valid throw an AccessError
-    if not database["tokens"].get(token, False):
-        raise AccessError(description="invalid token")
+    u_id = check_valid_token(token)
 
-    # get the user's u_id
-    token_payload = jwt.decode(token, get_secret(), algorithms=["HS256"])
-    u_id = token_payload["u_id"]
-
+    # use a quick list comprehension to get the channels that this u_id
+    # is listed as a member of
     channels = [
         c.frontend_format() for c in database["channels"] if u_id in c.get_members()
     ]
@@ -277,12 +246,15 @@ def channels_list(token):
 
 
 def channels_listall(token):
+    """
+    Return a list of all channels in the slackr.
+    """
+
     # this is pretty simple, just grab the "database"
     database = get_data()
 
-    # if the token is not valid throw an AccessError
-    if not database["tokens"].get(token, False):
-        raise AccessError(description="Invalid token")
+    # make sure the token is valid
+    check_valid_token(token)
 
     # quick little list comprehension to return the channels in the
     # format we need
