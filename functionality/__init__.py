1d81baef2fb5a2294697f05670ee3a5c88a47c9f
Owen Chandler <z5255778@unsw.edu.au>
2019-11-14 23:24:20 +1100

    __init__.py at 10/10 pylint

--- functionality/__init__.py
+++ functionality/__init__.py
@@ -1,7 +1,17 @@
-# sample code taken from: https://stackoverflow.com/questions/1057431/how-to-load-all-modules-in-a-folder
-# added a few changes to exclude test files
+# pylint: disable=C0103
+
+"""
+sample code taken from:
+https://stackoverflow.com/questions/1057431/how-to-load-all-modules-\
+in-a-folder
+added a few changes to exclude test files
+"""
+
 from os.path import dirname, basename, isfile, join
 import glob
+
+
 modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f) 
-            and not f.endswith('__init__.py') and not f.endswith("_test.py") ]
+
+__all__ = [basename(f)[:-3] for f in modules if isfile(f)
+           and not f.endswith('__init__.py') and not f.endswith("_test.py")]
