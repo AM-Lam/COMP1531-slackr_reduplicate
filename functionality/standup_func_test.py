18a972806f7795826d525782ab05a01b57adb5ce
Arpit <a.rulania@student.unsw.edu.au>
2019-11-16 14:44:34 +1100

    added decorators to standup_func_test.py

--- functionality/standup_func_test.py
+++ functionality/standup_func_test.py
@@ -9,103 +9,88 @@ from .auth import auth_register
 from .database import clear_data
 from .channel import channels_create
 from .access_error import AccessError, Value_Error
+from .decorators import setup_data
 
 #######################################################################
 ###  STANDUP_START TESTS HERE #########################################
 #######################################################################
 
-def test_standup_start():
-    clear_data()
-
-    user = auth_register("valid@email.com", "1234567890", "John", "Doe")
-    user2 = auth_register("valid2@email.com", "1234567890", "John", "Boe")
-    channel = channels_create(user["token"], "Channel 1", False)
-
+@setup_data(user_num=2, channel_num=1, public=[False])
+def test_standup_start(users, channels): 
     # this test should pass with no issue, to assert just check that the time
     # it returns is within some small range
     dev_time = 5
 
     predicted_finish_time = datetime.now() + timedelta(seconds=dev_time)
-    finish_time = standup_start(user["token"], channel["channel_id"], dev_time)
+    finish_time = standup_start(users[0]["token"], channels[0]["channel_id"], dev_time)
 
     assert predicted_finish_time - finish_time <= timedelta(6)
 
     # returns a Value_Error if the channel doesn't exist
-    pytest.raises(Value_Error, standup_start, user["token"],
+    pytest.raises(Value_Error, standup_start, users[0]["token"],
                   "not_a_real_channel", dev_time)
 
     # returns an AccessError if the user does not have perms
-    pytest.raises(AccessError, standup_start, user2["token"],
-                  channel["channel_id"], dev_time)
+    pytest.raises(AccessError, standup_start, users[1]["token"],
+                  channels[0]["channel_id"], dev_time)
 
 #######################################################################
 ###  STANDUP_SEND TESTS HERE ##########################################
 #######################################################################
 
-def test_standup_send():
-    clear_data()
-
+@setup_data(user_num=2, channel_num=1, public=[False])
+def test_standup_send(users, channels):
     dev_time = 5
-
-    user = auth_register("valid@email.com", "1234567890", "John", "Doe")
-    user2 = auth_register("valid2@email.com", "1234567890", "John", "Zoe")
-
-    channel = channels_create(user["token"], "Channel 1", False)
 
     predicted_finish = datetime.now() + timedelta(seconds=dev_time + 1)
 
     # start the standup
-    threading.Thread(target=standup_start, args=(user["token"],
-                     channel["channel_id"], dev_time)).start()
+    threading.Thread(target=standup_start, args=(users[0]["token"],
+                     channels[0]["channel_id"], dev_time)).start()
 
     # this test should pass with no issue
-    assert standup_send(user["token"], channel["channel_id"], "message") == {}
+    assert standup_send(users[0]["token"], channels[0]["channel_id"], "message") == {}
 
     # raises a Value_Error if channel does not exist
-    pytest.raises(Value_Error, standup_send, user["token"], "not_a_real_channel", "message")
+    pytest.raises(Value_Error, standup_send, users[0]["token"], "not_a_real_channel", "message")
 
     # raises an AccessError if the user does not have perms
-    pytest.raises(AccessError, standup_send, user2["token"], channel["channel_id"], "message")
+    pytest.raises(AccessError, standup_send, users[1]["token"], channels[0]["channel_id"], "message")
 
     # raises a Value_Error if the message is too long
-    pytest.raises(Value_Error, standup_send, user["token"], channel["channel_id"], "a" * 1001)
+    pytest.raises(Value_Error, standup_send, users[0]["token"], channels[0]["channel_id"], "a" * 1001)
 
     # if standup time has stopped
     while datetime.now() <= predicted_finish:
         continue
 
-    pytest.raises(AccessError, standup_send, user["token"], channel["channel_id"], "message")
+    pytest.raises(AccessError, standup_send, users[0]["token"], channels[0]["channel_id"], "message")
 
 #######################################################################
 ###  STANDUP_ACTIVE TESTS HERE ########################################
 #######################################################################
 
-def test_standup_active():
-    clear_data()
-
+@setup_data(user_num=1, channel_num=1, public=[False])
+def test_standup_active(users, channels):
     dev_time = 5
-
-    user = auth_register("valid@email.com", "1234567890", "John", "Doe")
-
-    channel = channels_create(user["token"], "Channel 1", False)
 
     predicted_finish = datetime.now() + timedelta(seconds=dev_time + 1)
 
     # start the standup
-    threading.Thread(target=standup_start, args=(user["token"],
-                     channel["channel_id"], dev_time)).start()
+    threading.Thread(target=standup_start, args=(users[0]["token"],
+                     channels[0]["channel_id"], dev_time)).start()
 
     # this test should pass with no issue
-    new_standup = standup_active(user["token"], channel["channel_id"])
+    new_standup = standup_active(users[0]["token"], channels[0]["channel_id"])
     assert new_standup["is_active"]
     assert new_standup["time_finish"] - predicted_finish <= timedelta(seconds=1)
 
     # raises a Value_Error if channel does not exist
-    pytest.raises(Value_Error, standup_active, user["token"], "not_a_real_channel")
+    pytest.raises(Value_Error, standup_active, users[0]["token"], "not_a_real_channel")
 
     # if standup time has stopped
     while datetime.now() <= predicted_finish:
         continue
 
-    assert standup_active(user["token"], channel["channel_id"]) == \
+    assert standup_active(users[0]["token"], channels[0]["channel_id"]) == \
     {"is_active" : False, "time_finish" : None}
