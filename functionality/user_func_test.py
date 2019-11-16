2607be102ece31f1c3075bfcfd4d0bf5c08c20a8
Arpit <a.rulania@student.unsw.edu.au>
2019-11-16 14:19:47 +1100

    added some more tests to user_func_test.py and added decorators to unclutter space

--- functionality/user_func_test.py
+++ functionality/user_func_test.py
@@ -10,70 +10,63 @@ from .user import (user_profile_setemail, user_profile_sethandle,
 from .auth import auth_register
 from .database import clear_data, get_user
 from .access_error import Value_Error
+from .decorators import setup_data
 
 
 #######################################################################
 ###  USER_PROFILE_SETEMAIL TESTS HERE #################################
 #######################################################################
 
-def test_user_profile_setemail():
-    clear_data()
-    user1 = auth_register("valid@email.com", "1234567890", "John", "Doe")
-    user2 = auth_register("valid2@email.com", "1234567890", "John", "Zed")
-
+@setup_data(user_num=2)
+def test_user_profile_setemail(users, channels):
     # this test should pass with no issue
-    assert user_profile_setemail(user1["token"], "z1234567@cse.unsw.edu.au") == {}
+    assert user_profile_setemail(users[0]["token"], "z1234567@cse.unsw.edu.au") == {}
 
     # if the email doesnt exist or is invalid
-    pytest.raises(Value_Error, user_profile_setemail, user1["token"], "thisisjustastring")
+    pytest.raises(Value_Error, user_profile_setemail, users[0]["token"], "thisisjustastring")
 
     # if the email is used by another user (check the site)
-    pytest.raises(Value_Error, user_profile_setemail, user2["token"], "z1234567@cse.unsw.edu.au")
+    pytest.raises(Value_Error, user_profile_setemail, users[1]["token"], "z1234567@cse.unsw.edu.au")
 
 
 #######################################################################
 ###  USER_PROFILE_SETHANDLE TESTS HERE ################################
 #######################################################################
 
-def test_user_profile_sethandle():
-    clear_data()
-    user1 = auth_register("valid@email.com", "1234567890", "John", "Doe")
-    user2 = auth_register("valid2@email.com", "1234567890", "Bob", "John")
-
+@setup_data(user_num=2)
+def test_user_profile_sethandle(users, channels):
     # this test should pass with no issue
-    assert user_profile_sethandle(user1["token"], "handle") == {}
+    assert user_profile_sethandle(users[0]["token"], "handle") == {}
 
     # return a Value_Error if the handle is too long
-    pytest.raises(Value_Error, user_profile_sethandle, user1["token"], "abcdefghijklmnopqrstuvwxyz")
+    pytest.raises(Value_Error, user_profile_sethandle, users[0]["token"], "abcdefghijklmnopqrstuvwxyz")
 
     # if the handle (tested by "handle1") is already in use
-    pytest.raises(Value_Error, user_profile_sethandle, user2["token"], "handle")
+    pytest.raises(Value_Error, user_profile_sethandle, users[1]["token"], "handle")
 
 
 #######################################################################
 ###  USER_PROFILE_SETNAME TESTS HERE ##################################
 #######################################################################
 
-def test_user_profile_setname():
-    clear_data()
-    user = auth_register("valid@email.com", "1234567890", "John", "Doe")
-
+@setup_data(user_num=1)
+def test_user_profile_setname(users, channels):
     # this test should pass with no issue
-    assert user_profile_setname(user["token"], "Jane", "Smith") == {}
+    assert user_profile_setname(users[0]["token"], "Jane", "Smith") == {}
 
     # trying to input a first name longer than 50 characters
-    pytest.raises(Value_Error, user_profile_setname, user["token"],
+    pytest.raises(Value_Error, user_profile_setname, users[0]["token"],
                   "a" * 51, "Smith")
 
     # trying to input a last name longer than 50 characters
-    pytest.raises(Value_Error, user_profile_setname, user["token"],
+    pytest.raises(Value_Error, user_profile_setname, users[0]["token"],
                   "Jane", "a" * 51)
 
     # assuming we allow mononymous names, at least one value needs to be filled
-    assert user_profile_setname(user["token"], "Plato", "") == {}
+    assert user_profile_setname(users[0]["token"], "Plato", "") == {}
 
     # trying to input an empty name
-    pytest.raises(Value_Error, user_profile_setname, user["token"], "", "")
+    pytest.raises(Value_Error, user_profile_setname, users[0]["token"], "", "")
 
 
 #######################################################################
@@ -87,22 +80,20 @@ def verify_info1(user_obj, correct_data):
         return True
     return False
 
-def test_user_profile1():
-    clear_data()
-    user1 = auth_register("valid@email.com", "1234567", "Bob", "Jones")
-
+@setup_data(user_num=1)
+def test_user_profile1(users, channels):
     # try to create a valid message
-    profile = user_profile(user1["token"], 1)
+    profile = user_profile(users[0]["token"], 1)
 
     # check that the user exists
     assert profile is not None
 
     # check that the database was correctly updated
     assert profile == {
-        'email': "valid@email.com",
-        'name_first': "Bob",
-        'name_last': "Jones",
-        'handle_str': "BobJones"
+        'email': "user1@valid.com",
+        'name_first': "user1",
+        'name_last': "last1",
+        'handle_str': "user1last1"
         }
 
 
@@ -110,46 +101,40 @@ def test_user_profile1():
 ###  USER_PROFILE_UPLOAD_PHOTO TESTS HERE #############################
 #######################################################################
 
-def test_user_profiles_uploadphoto():
-    clear_data()
-
-    user = auth_register("valid@email.com", "1234567890", "John", "Doe")
-
+@setup_data(user_num=1)
+def test_user_profiles_uploadphoto(users, channels):
     # NOTE: for an image with width 200, end co-ordinates must be 199
     sample = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/16x16%2BR.jpg/256px-16x16%2BR.jpg"
 
     # checking for invalid URL
-    pytest.raises(ValueError, user_profiles_uploadphoto, user["token"], "cseunsw.edu.au", 0, 0, 199, 199)
+    pytest.raises(ValueError, user_profiles_uploadphoto, users[0]["token"], "cseunsw.edu.au", 0, 0, 199, 199)
 
     # checking if coordinates are valid
-    pytest.raises(Value_Error, user_profiles_uploadphoto, user["token"], sample, -1, -1, 200, 200)
+    pytest.raises(Value_Error, user_profiles_uploadphoto, users[0]["token"], sample, -1, -1, 200, 200)
 
     # checking if cropping area is too big for the image
-    pytest.raises(Value_Error, user_profiles_uploadphoto, user["token"], sample, 0, 0, 300, 300)
+    pytest.raises(Value_Error, user_profiles_uploadphoto, users[0]["token"], sample, 0, 0, 300, 300)
 
     # checking sequentialism
-    pytest.raises(Value_Error, user_profiles_uploadphoto, user["token"], sample, 20, 20, 10, 10)
+    pytest.raises(Value_Error, user_profiles_uploadphoto, users[0]["token"], sample, 20, 20, 10, 10)
 
     # checking if selection is a square
-    pytest.raises(Value_Error, user_profiles_uploadphoto, user["token"], sample, 0, 0, 199, 179)
-    pytest.raises(Value_Error, user_profiles_uploadphoto, user["token"], sample, 50, 0, 199, 199)
+    pytest.raises(Value_Error, user_profiles_uploadphoto, users[0]["token"], sample, 0, 0, 199, 179)
+    pytest.raises(Value_Error, user_profiles_uploadphoto, users[0]["token"], sample, 50, 0, 199, 199)
 
     # this test should pass with no issue
-    assert user_profiles_uploadphoto(user["token"], sample, 0, 0, 199, 199) == {}
+    assert user_profiles_uploadphoto(users[0]["token"], sample, 0, 0, 199, 199) == {}
 
 #######################################################################
 ###  USERS_ALL TESTS HERE #############################################
 #######################################################################
 
-def test_users_all():
-    clear_data()
-    user1 = auth_register("valid@email.com", "1234567", "Bob", "Jones")
-    user2 = auth_register("valid1@email.com", "11221122", "Sally", "Salmon")
-
-    user_t = get_user(user1['u_id'])
-    user_u = get_user(user2['u_id'])
-    # testing good functionality:
-    assert users_all(user1['token']) == {'users':['BobJones','SallySalmon']}
+@setup_data(user_num=3)
+def test_users_all(users, channels):
+    # testing good functionality (all users should have access):
+    assert users_all(users[0]['token']) == {'users':['user1last1','user2last2', 'user3last3']}
+    assert users_all(users[1]['token']) == {'users':['user1last1','user2last2', 'user3last3']}
+    assert users_all(users[2]['token']) == {'users':['user1last1','user2last2', 'user3last3']}
     # invalid token test:
     pytest.raises(Value_Error, users_all, 'fjngnbfdk')
     
